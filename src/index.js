53addb116c0f78bbb99376295e769b4f94e36d73
Wilson Gouanet <wilstito@gmail.com>
2020-05-26 21:19:34 +0200

    reorganise root component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import theme from './theme';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={ theme }>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
