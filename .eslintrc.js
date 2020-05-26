43063790d819877832850ef62287b23cd130c28e
Wilson Gouanet <wilstito@gmail.com>
2020-05-26 21:57:30 +0200

    Configure eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {},
+  extends: ["plugin:prettier/recommended"],
+}
