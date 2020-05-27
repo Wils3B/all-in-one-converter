d1590d248c814891e718372f76a29deb4e624d65
Wilson Gouanet <wilstito@gmail.com>
2020-05-27 17:16:47 +0200

    add react hooks eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,22 @@ module.exports = {
     commonjs: true,
     es6: true,
   },
-  extends: ["plugin:react/recommended", "airbnb"],
+  extends: ['plugin:react/recommended', 'airbnb'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 11,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react"],
-  rules: {},
-  extends: ["plugin:prettier/recommended"],
-}
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  extends: ['plugin:prettier/recommended'],
+};
