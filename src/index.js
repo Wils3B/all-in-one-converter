365a154c4c6ec6bc2ad4a7d0f4bcda7eb364cf1f
Wilson Gouanet <wilstito@gmail.com>
2020-05-30 00:12:00 +0200

    create root component and implement theme toggling

--- src/index.js
+++ src/index.js
@@ -2,17 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import theme from './theme';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import themes from './themes';
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
-);
+export const ThemeContext = React.createContext({
+  name: 'light',
+  toggleTheme: () => {},
+});
+
+function RootComponent(props) {
+  const themeStyle = React.useContext(ThemeContext);
+  const [theme, setTheme] = React.useState(themeStyle.name);
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <ThemeProvider theme={themes[theme]}>
+      <CssBaseline />
+      <ThemeContext.Provider value={{ name: theme, toggleTheme }}>
+        <App />
+      </ThemeContext.Provider>
+    </ThemeProvider>
+  );
+}
+
+ReactDOM.render(<RootComponent />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
