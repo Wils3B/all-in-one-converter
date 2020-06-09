4cbe11b67d2777dacf2b1adf2658047477f0876f
Wilson Gouanet <wilstito@gmail.com>
2020-06-10 01:16:03 +0200

    feat: inegrate the router inside app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import TopBar from './components/TopBar';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import AppMenu from './pages/Menu.page';
 
 const App = function (props) {
   return (
     <React.Fragment>
       <TopBar />
+      <Router>
+        <Switch>
+          <Route path="/menu" component={AppMenu} />
+          <Redirect to="/menu" />
+        </Switch>
+      </Router>
     </React.Fragment>
   );
 };
