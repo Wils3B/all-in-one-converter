58eac8a4cf212e69b2fbd2c69963e0aa9c8c9407
Wilson Gouanet <wilstito@gmail.com>
2020-06-11 01:13:11 +0200

    feat: add AngleConverterPage to main routing pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import TopBar from './components/TopBar';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import AppMenu from './pages/Menu.page';
+import MenuPage from './pages/Menu.page';
+import AnglesPage from './pages/Angles.page';
 
 const App = function (props) {
   return (
@@ -9,7 +10,8 @@ const App = function (props) {
       <TopBar />
       <Router>
         <Switch>
-          <Route path="/menu" component={AppMenu} />
+          <Route path="/menu" component={MenuPage} />
+          <Route path="/angles" component={AnglesPage} />
           <Redirect to="/menu" />
         </Switch>
       </Router>
